69b61ddd450922c646975effab067fca5237a464
Valentin Bersier <vbersier@gmail.com>
2023-04-05 09:54:51 +0200

    feat: remove the fluff

--- src/App.svelte
+++ src/App.svelte
@@ -4,11 +4,4 @@
 <main class="container">Hi</main>
 
 <style>
-  .logo.vite:hover {
-    filter: drop-shadow(0 0 2em #747bff);
-  }
-
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00);
-  }
 </style>
