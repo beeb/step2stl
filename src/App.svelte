b1a4ff1222a5f532cbbc968c5b7d153bdd00e39c
Valentin Bersier <vbersier@gmail.com>
2023-04-05 10:26:02 +0200

    fix: calling command

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,22 @@
 <script lang="ts">
   import { listen } from '@tauri-apps/api/event'
+  import { invoke } from '@tauri-apps/api/tauri'
 
-  listen('tauri://file-drop', (event) => {
+  let error = {}
+
+  listen('tauri://file-drop', async (event) => {
     const path = event.payload[0]
+    try {
+      await invoke('convert', { path, chordError: 0.005, angleRes: 1.0 })
+    } catch (e) {
+      error = e
+    }
   })
 </script>
 
-<main class="container">Hi</main>
+<main class="container">
+  {JSON.stringify(error)}
+</main>
 
 <style>
 </style>
