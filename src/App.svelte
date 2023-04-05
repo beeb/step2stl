6066fb9127f4366db28194a47c9999b82895e133
Valentin Bersier <vbersier@gmail.com>
2023-04-05 10:09:16 +0200

    feat: file drop

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,9 @@
 <script lang="ts">
+  import { listen } from '@tauri-apps/api/event'
+
+  listen('tauri://file-drop', (event) => {
+    const path = event.payload[0]
+  })
 </script>
 
 <main class="container">Hi</main>
