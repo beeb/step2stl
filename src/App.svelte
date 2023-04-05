ace4c7f08b357a505e01cf57bb6d3568e2637f7e
Valentin Bersier <vbersier@gmail.com>
2023-04-05 15:52:11 +0200

    feat: add some UI

--- src/App.svelte
+++ src/App.svelte
@@ -7,6 +7,11 @@
 
   listen('tauri://file-drop', async (event) => {
     const path = event.payload[0]
+    const extension = path.split('.').pop()
+    if (extension.toLowerCase() !== 'step') {
+      error = { message: 'Only STEP files are supported' }
+      return
+    }
     try {
       await invoke('convert', { path, chordError: 0.005, angleRes: 1.0 })
     } catch (e) {
@@ -15,10 +20,10 @@
   })
 </script>
 
-<main class="container">
-  <h1 class="text-xl">Hi</h1>
-  <div class="mockup-code">
-    <pre><code>{JSON.stringify(error)}</code></pre>
+<main class="p-6 w-full h-screen flex flex-col gap-6">
+  <h1 class="text-xl text-center">STEP to STL converter</h1>
+  <div class="card bg-base-300 shadow-xl flex-grow">
+    <div class="card-body flex items-center justify-center">Drop a file here to convert</div>
   </div>
 </main>
 
