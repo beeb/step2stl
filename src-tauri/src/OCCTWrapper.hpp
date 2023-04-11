4db7f95dfb1352496790e9594b341d3833c4a879
Valentin Bersier <vbersier@gmail.com>
2023-04-11 17:16:25 +0200

    style: comment

--- src-tauri/src/OCCTWrapper.hpp
+++ src-tauri/src/OCCTWrapper.hpp
@@ -9,6 +9,6 @@ namespace OcctWrapper
 
     bool convert_step_to_stl(rust::String step_file_path, rust::String stl_file_path, rust::f64 chord_error, rust::f64 angle_res);
 
-}; // namespace MyTest
+}; // namespace OcctWrapper
 
 #endif // occtwrapper_OCCTWrapper_hpp_
