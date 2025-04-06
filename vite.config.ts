b866911668ca5179910dac664ea0ecdba48ee622
Valentin B. <703631+beeb@users.noreply.github.com>
2025-04-06 16:51:01 +0200

    chore(deps): update deps (#10)
    * chore(deps): update deps
    
    * docs: update screenshot
    
    * chore: bump version

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    svelte()
+    tailwindcss(),
+    svelte(),
   ],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
